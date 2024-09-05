d20e711a5dcc0d97e5591062405fb9eca1b03647
chasinghope <42870306+chasinghope@users.noreply.github.com>
2024-09-05 09:06:06 +0800

    Widget

---
+++ Runtime/Widget/Text/TextStyleManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Knight.Core;
+using UnityEngine;
+
+namespace UnityEngine.UI
+{
+    [ExecuteInEditMode]
+    public class TextStyleManager : MonoBehaviour
+    {
+        private static TextStyleManager __instance;
+        public  static TextStyleManager Instance { get { return __instance; } }
+
+        public List<TextStyle>          TextStyles;
+
+        private void Awake()
+        {
+            if (__instance != null)
+                __instance = this;
+        }
+    }
+}
